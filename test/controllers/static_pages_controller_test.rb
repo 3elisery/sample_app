cbae01824ba7a4c875e25534a7409d1b3e5d21ab
3elisery <elissermakova@gmail.com>
2024-05-09 22:46:30 +0300

    Improve formatting of title assertions in tests

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -8,19 +8,19 @@ def setup
   test "should get home" do
     get root_path
     assert_response :success
-  assert_select "title", "#{@base_title}"
+    assert_select "title", "#{@base_title}"
   end
 
   test "should get help" do
     get help_path
     assert_response :success
-  assert_select "title", "Help | #{@base_title}"
+    assert_select "title", "Help | #{@base_title}"
   end
 
   test "should get about" do
     get about_path
     assert_response :success
-  assert_select "title", "About | #{@base_title}"
+    assert_select "title", "About | #{@base_title}"
   end
 
   test "should get contact" do
