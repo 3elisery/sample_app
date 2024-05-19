7a40e5ff2bfe5747d8904352d698e12f6a85d44c
3elisery <elissermakova@gmail.com>
2024-05-19 16:54:03 +0300

    Include password and confirmation in user test setup

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -3,7 +3,8 @@ require "test_helper"
 class UserTest < ActiveSupport::TestCase
   
   def setup 
-    @user = User.new(name: "Example User", email: "user@example.com")
+    @user = User.new(name: "Example User", email: "user@example.com", password: "foobar", password_confirmation: "foobar")
+
   end
 
   test "should be valid" do
@@ -32,9 +33,22 @@ class UserTest < ActiveSupport::TestCase
 
   test "email validation should accept valid addresses" do 
     invalid_addresses = %w[user@example,com user_at_foo.org user.name@example foo@bar_baz.com foo@bar+baz.com  foo@bar..com]
-      invalid_addresses.each do |invalid_address|
-        @user.email = invalid_address
-        assert_not @user.valid?, "#{invalid_address.inspect} should be valid"
+    invalid_addresses.each do |invalid_address|
+      @user.email = invalid_address
+      assert_not @user.valid?, "#{invalid_address.inspect} should be valid"
       end
     end
+    
+  test "email addresses should be unique" do 
+    duplicate_user = @user.dup 
+    @user.save 
+    assert_not duplicate_user.valid? 
+  end
+
+  test "email addresses should be saved as lowercase" do
+    wrong_email = "fOO@mMM.CoM"
+    @user.email = wrong_email
+    @user.save
+    assert_equal wrong_email.downcase, @user.reload.email
+  end
 end
